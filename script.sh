0f662638bbb69686c88a57f6b782bff28fa4c8ac
Umair Ahmed Khan <umairkhan.cis@gmail.com>
2022-07-18 17:38:55 +0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+
+echo What is in this repository?
+ls -a
+echo 
+echo is Java installed?
+java --version
+echo 
+echo is Git installed?
+git --version
+echo 
+echo What about build tools?
+mvn --version
+echo
+gradle --version
+echo
+ant -version
+env
